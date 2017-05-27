acedde6538dc7b732d3a8f5772271f902ca1b8ce
liquid8d <liquid8d@gmail.com>
2017-05-27 12:21:30 -0400

    set default charsize until we add auto-height for text

--- js/am-listbox.js
+++ js/am-listbox.js
@@ -23,7 +23,7 @@ function AMListBox(x, y, width, height) {
         selbg_blue: { label: 'selbg_blue', type: 'number', default: 255, min: 0, max: 255 },
         selbg_alpha: { label: 'selbg_alpha', type: 'number', default: 255, min: 0, max: 255 },
         align: { label: 'align', type: 'select', default: 'Align.Left', values: [ 'Align.Left', 'Align.Centre', 'Align.Right' ]},
-        charsize: { label: 'charsize', type: 'number', default: -1, min: -1, max: 100 },
+        charsize: { label: 'charsize', type: 'number', default: 16, min: -1, max: 100 },
         font: { label: 'font', type: 'text', default: 'Arial' },
         rows: { label: 'rows', type: 'number', default: 11 },
         format_string: { label: 'format_string', type: 'text', default: '[Title]' }
