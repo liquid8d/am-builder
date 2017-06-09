348828d3e08fced92b904f75cbc279b926feea6c
liquid8d <liquid8d@gmail.com>
2017-06-09 16:47:20 -0400

    text fixes  - default to Align.Centre - autosize text with -1 charsize  - moved text span into its own div

--- js/objects/am-text.js
+++ js/objects/am-text.js
@@ -8,7 +8,7 @@ class AMText extends AMObject {
             msg: { label: 'msg', type: 'dropdown', default: '[Title]', values: [ '[DisplayName]', '[ListSize]', '[ListEntry]', '[FilterName]', '[Search]', '[SortName]', '[Name]', '[Title]', '[Emulator]', '[CloneOf]', '[Year]', '[Manufacturer]', '[Category]', '[Players]', '[Rotation]', '[Control]', '[Status]', '[DisplayCount]', '[DisplayType]', '[AltRomname]', '[AltTitle]', '[PlayedTime]', '[PlayedCount]', '[SortValue]', '[System]', '[SystemN]', '[Overview]' ] },
             font: { label: 'font', type: 'file', default: '', values: 'font' },
             charsize: { label: 'charsize', type: 'number', default: 16, min: -1, max: 100 },
-            align: { label: 'align', type: 'select', default: 'Align.Left', values: [ 'Align.Left', 'Align.Centre', 'Align.Right' ] },
+            align: { label: 'align', type: 'select', default: 'Align.Centre', values: [ 'Align.Left', 'Align.Centre', 'Align.Right' ] },
             style: { label: 'style', type: 'multiselect', default: 0, values: [ { label: 'Bold', value: 1, checked: [ 1,3,5,7 ] }, { label: 'Italic', value: 2, checked: [2,3,6,7] }, { label: 'Underlined', value: 4, checked: [4,5,6,7] } ] },
             word_wrap: { label: 'word_wrap', type: 'bool', default: false },
             red: { label: 'red', type: 'range', default: 255, min: 0, max: 255 },
@@ -33,32 +33,66 @@ class AMText extends AMObject {
     }
 
     createElement() {
-        this.el = document.createElement('span')
+        this.el = document.createElement('div')
         this.el.classList.add('text')
         this.el.style.overflow = 'hidden'
+        var text = document.createElement('span')
+        text.style.display = 'inline-block'
+        text.style.position = 'relative'
+        text.style.overflow = 'hidden'
+        text.style.pointerEvents = 'none'
+        this.el.appendChild(text)
     }
 
     updateElement() {
         this.el.style.display = ( this.values.visible && !this.editor.hidden ) ? ( this.values.word_wrap ) ? 'inline-block' : 'block' : 'none'
         if ( this.values.zorder >= 0 ) this.el.style.zIndex = this.values.zorder
-        this.el.innerHTML = this.magicTokens( this.values.msg, this.values.index_offset )
 
-        //update object transform
-        this.transform()
+        var text = this.el.querySelector('span')
+        text.innerHTML = this.magicTokens( this.values.msg, this.values.index_offset )
+        
+        var bg_alpha = ( this.values.bg_alpha > 0 ) ? this.values.bg_alpha / 255 : 0
+        this.el.style.backgroundColor = 'rgba(' + this.values.bg_red + ',' + this.values.bg_green + ',' + this.values.bg_blue + ',' + bg_alpha + ')'
+        
+        var alpha = ( this.values.alpha > 0 ) ? this.values.alpha / 255 : 0
+        text.style.color = 'rgba(' + this.values.red + ',' + this.values.green + ',' + this.values.blue + ', ' + alpha + ')'
+        text.style.fontFamily = this.values.font
+        text.style.fontWeight = ( this.props.style.values[0].checked.includes(this.values.style) ) ? 'bold' : 'normal'
+        text.style.fontStyle = ( this.props.style.values[1].checked.includes(this.values.style) ) ? 'italic' : ''
+        text.style.textDecoration = ( this.props.style.values[2].checked.includes(this.values.style) ) ? 'underline' : ''
+        text.style.wordBreak = 'break-all'
+
         this.el.style.width = this.values.width + 'px'
         this.el.style.height = this.values.height + 'px'
 
-        var alpha = ( this.values.alpha > 0 ) ? this.values.alpha / 255 : 0
-        this.el.style.color = 'rgba(' + this.values.red + ',' + this.values.green + ',' + this.values.blue + ', ' + alpha + ')'
-        var bg_alpha = ( this.values.bg_alpha > 0 ) ? this.values.bg_alpha / 255 : 0
-        this.el.style.backgroundColor = 'rgba(' + this.values.bg_red + ',' + this.values.bg_green + ',' + this.values.bg_blue + ',' + bg_alpha + ')'
+        //do transform after all properties are set, since some may affect it
+        this.transform()
+    }
+
+    transform() {
+        super.transform()
+        var text = this.el.querySelector('span')
+        if ( this.values.charsize != -1 ) {
+            if ( this.values.word_wrap ) {
+                text.style.fontSize = this.values.charsize + 'px'
+                //center vertically
+                text.style.lineHeight = 'normal'
+                //this doesn't quite center properly for charsize of 9 or less
+                text.style.top = ( ( this.el.getBoundingClientRect().height - text.getBoundingClientRect().height ) / 2 ) + 'px'
+            } else {
+                //specific font size
+                text.style.fontSize = this.values.charsize + 'px'
+                //center vertically
+                text.style.lineHeight = this.values.height + 'px'
+                text.style.top = 0
+            }
+        } else {
+            //auto size text (no word wrap)
+            text.style.top = 0
+            text.style.fontSize = this.values.height + 'px'
+            text.style.lineHeight = ( this.values.height * 1.0 ) + 'px'
+        }
         this.el.style.textAlign = ( this.values.align == 'Align.Centre' ) ? 'center' : ( this.values.align == 'Align.Right' ) ? 'right' : 'left'
-        this.el.style.fontFamily = this.values.font
-        this.el.style.fontSize = ( this.values.charsize != -1 ) ? this.values.charsize + 'px' : '100vh'
-        this.el.style.whiteSpace = ( this.values.word_wrap ) ? 'normal' : 'nowrap'
-        this.el.style.fontWeight = ( this.props.style.values[0].checked.includes(this.values.style) ) ? 'bold' : 'normal'
-        this.el.style.fontStyle = ( this.props.style.values[1].checked.includes(this.values.style) ) ? 'italic' : ''
-        this.el.style.textDecoration = ( this.props.style.values[2].checked.includes(this.values.style) ) ? 'underline' : ''
     }
 
     toSquirrel() {
